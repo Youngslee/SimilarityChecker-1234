106ca4ad6d27bec6996b425033d6ebc93bdc9e6b
youngslee <popopo3204@naver.com>
2023-07-06 11:39:02 +0900

    [feature] 4

--- Project15/Cal.cpp
+++ Project15/Cal.cpp
@@ -3,19 +3,22 @@ using namespace std;
 class Cal
 {
 public:
-	int getLengthPoint(const string & input1, const string & input2)
+	int getPartialPoints(const string& input1, const string& input2)
 	{
 		int gap = input1.size() - input2.size();
-		if(gap == 0)
-		{
-			return SAME_LENGTH_MAX_SCORE;
-		}
-
 		int shortLength = input1.size();
 		if (gap > 0) shortLength = input2.size();
 
-		int result = (1 - ((double)abs(gap) / (double)shortLength)) * 60;
-		return result;
+		return (1 - ((double)abs(gap) / (double)shortLength)) * SAME_LENGTH_MAX_SCORE;
+	}
+
+	int getLengthPoint(const string & input1, const string & input2)
+	{
+		if(input1.size() - input2.size() == 0)
+		{
+			return SAME_LENGTH_MAX_SCORE;
+		}
+		return getPartialPoints(input1, input2);
 	}
 
 private:
