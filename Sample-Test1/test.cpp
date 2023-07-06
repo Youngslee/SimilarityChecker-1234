b0b18b54dacf01fac35f9b4c155a704871161643
youngslee <popopo3204@naver.com>
2023-07-06 11:46:09 +0900

    [refactoring] 6

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,26 +1,23 @@
 #include "pch.h"
 #include "../project15/Cal.cpp"
 TEST(TestCaseName, getReturnSameLengthPoint) {
+	Cal cal;
 	string input1 = "ABC";
 	string input2 = "BBY";
-	Cal cal;
-	int ret = cal.getLengthPoint(input1, input2);
-	EXPECT_EQ(60, ret);
+	EXPECT_EQ(60, cal.getLengthPoint(input1, input2));
 }
 
 TEST(TestCaseName, getReturnPartialPoint) {
+	Cal cal;
 	string input1 = "AAABB";
 	string input2 = "BAA";
-	Cal cal;
-	int ret = cal.getLengthPoint(input1, input2);
-	EXPECT_EQ(20, ret);
+	EXPECT_EQ(20, cal.getLengthPoint(input1, input2));
 }
 
 TEST(TestCaseName, getReturnZeroPoint) {
+	Cal cal;
 	string input1 = "A";
 	string input2 = "BBB";
-	Cal cal;
-	int ret = cal.getLengthPoint(input1, input2);
-	EXPECT_EQ(0, ret);
+	EXPECT_EQ(0, cal.getLengthPoint(input1, input2));
 }
 
