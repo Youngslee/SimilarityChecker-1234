381f240246a6afe650dc8cdeaf4798e146ae6aad
youngslee <popopo3204@naver.com>
2023-07-06 13:12:32 +0900

    [feature] 1

---
+++ Project15/Cal.cpp
@@ -0,0 +1,10 @@
+#include <xstring>
+using namespace std;
+class Cal
+{
+public:
+	int getAlphaPoints(const string& input1, const string& input2)
+	{
+		return 40;
+	}
+};
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,10 @@
 #include "pch.h"
+#include "../Project15/Cal.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TestCaseName, GetReturnSameAlphas) {
+	string input1 = "ASD";
+	string input2 = "DSA";
+	Cal cal;
+	int ret = cal.getAlphaPoints(input1, input2);
+	EXPECT_EQ(40, ret);
 }
